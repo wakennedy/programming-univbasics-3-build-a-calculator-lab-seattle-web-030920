d7b6a352514284adcc51af909f3d5c3af97607fe
William Kennedy <alexkenned@gmail.com>
2020-02-12 21:32:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+  sum = num1 - num2
+  sum
 end
 
 def division(num1, num2)
-
+  sum = num1 / num2
+  sum
 end
 
 def multiplication(num1, num2)
-
+  sum = num1 * num2
+  sum
 end
 
 def modulo(num1, num2)
-
+  sum = num1 % num2
+  sum
 end
 
 def square_root(num)
-
+  sum = Math.sqrt(num)
+  sum
 end
